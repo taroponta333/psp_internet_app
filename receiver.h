78743f6ee81d0597fa292f86e9d5aff6b5dd89bd
taroponta333 <taroponta.333@gmail.com>
2026-07-11 13:06:34 +0900

    Create receiver.h

---
+++ receiver.h
@@ -0,0 +1,17 @@
+#ifndef RECEIVER_H
+#define RECEIVER_H
+
+/*=========================================================
+    Receiver
+=========================================================*/
+
+/* 初期化 */
+int Receiver_Init(void);
+
+/* 更新 */
+void Receiver_Update(void);
+
+/* 終了 */
+void Receiver_Shutdown(void);
+
+#endif
