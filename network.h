4b9b212b62250f232c5d24dd2f01041a0cd4c542
taroponta333 <taroponta.333@gmail.com>
2026-07-14 16:27:06 +0900

    Update network.h

--- network.h
+++ network.h
@@ -1,51 +1,52 @@
 /*
  * =========================================================
  * 3DS App Receiver v0.5.6
- * Network Header
- * Based on PSPSDK Official Sample
+ * Dialog Header
+ * Based on PSPSDK Official Samples
  * =========================================================
  */
 
-#ifndef __NETWORK_H__
-#define __NETWORK_H__
+#ifndef __DIALOG_H__
+#define __DIALOG_H__
 
 #include <pspkernel.h>
+#include <psputility.h>
+#include <psputility_netconf.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /*=========================================================
-    Network Initialize / Shutdown
+    Dialog Result
 =========================================================*/
 
-/* ネットワークライブラリ初期化 */
-int Network_Init(void);
-
-/* ネットワーク終了処理 */
-void Network_Shutdown(void);
+#define DIALOG_RESULT_RUNNING     1
+#define DIALOG_RESULT_OK          0
+#define DIALOG_RESULT_CANCEL     -1
+#define DIALOG_RESULT_ERROR      -2
 
 /*=========================================================
-    Connection Information
+    Network Configuration Dialog
 =========================================================*/
 
-/* 接続状態表示 */
-void Network_PrintConnectionState(void);
+/* 初期化 */
+int Dialog_Init(void);
 
-/* IPアドレス取得 */
-int Network_GetIP(char *ip);
+/* 更新 */
+int Dialog_Update(void);
 
-/* IPアドレス表示 */
-void Network_PrintIP(void);
+/* 終了処理 */
+void Dialog_Shutdown(void);
 
-/* 接続済みか判定
-   1 = Connected
-   0 = Not Connected
-*/
-int Network_IsConnected(void);
+/* 完了確認 */
+int Dialog_Finished(void);
+
+/* ネットワーク設定ダイアログ表示 */
+int Dialog_ShowNetConfig(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __NETWORK_H__ */
+#endif /* __DIALOG_H__ */
