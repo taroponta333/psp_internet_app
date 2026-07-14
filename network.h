4a396958c546c0703aebf4e19531b6c56dd010b8
taroponta333 <taroponta.333@gmail.com>
2026-07-14 16:20:12 +0900

    Update network.h

--- network.h
+++ network.h
@@ -1,26 +1,51 @@
-#ifndef NETWORK_H
-#define NETWORK_H
+/*
+ * =========================================================
+ * 3DS App Receiver v0.5.6
+ * Network Header
+ * Based on PSPSDK Official Sample
+ * =========================================================
+ */
+
+#ifndef __NETWORK_H__
+#define __NETWORK_H__
+
+#include <pspkernel.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /*=========================================================
-    Network
+    Network Initialize / Shutdown
 =========================================================*/
 
-/* 初期化 */
+/* ネットワークライブラリ初期化 */
 int Network_Init(void);
 
-/* 終了 */
+/* ネットワーク終了処理 */
 void Network_Shutdown(void);
 
-/* 接続状態 */
-int Network_IsConnected(void);
+/*=========================================================
+    Connection Information
+=========================================================*/
 
-/* 状態表示 */
+/* 接続状態表示 */
 void Network_PrintConnectionState(void);
 
-/* IP取得 */
+/* IPアドレス取得 */
 int Network_GetIP(char *ip);
 
-/* IP表示 */
+/* IPアドレス表示 */
 void Network_PrintIP(void);
 
+/* 接続済みか判定
+   1 = Connected
+   0 = Not Connected
+*/
+int Network_IsConnected(void);
+
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* __NETWORK_H__ */
